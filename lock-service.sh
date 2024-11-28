f6ba3d51cb2c3f8b5da28a6fa5a25fb3a6182993
LunatiX-nc <lunatixnc14@gmail.com>
2024-11-28 14:42:26 +0700

    Update lock-service.sh

--- lock-service.sh
+++ lock-service.sh
@@ -9,6 +9,22 @@ wget -q -O /etc/systemd/system/lock-tro.service "${REPO}files/lock-tro.service"
 wget -q -O /etc/systemd/system/lock-ssr.service "${REPO}files/lock-ssr.service"
 wget -q -O /etc/systemd/system/lock-ssh.service "${REPO}files/lock-ssh.service"
 
+sudo systemctl daemon-reload
+sudo systemctl restart lock-vme
+sudo systemctl restart lock-vle
+sudo systemctl restart lock-tro
+sudo systemctl restart lock-ssr
+sudo systemctl restart lock-ssh
+
+sudo systemctl enable lock-vme
+sudo systemctl enable lock-vle
+sudo systemctl enable lock-tro
+sudo systemctl enable lock-ssr
+sudo systemctl enable lock-ssh
+
+
+
+
 
 
 
